c6edfc1083710e6646535483bf0888073fcf7f9a
Vincent329 <vincee.ho@gmail.com>
2021-09-20 14:39:52 -0400

    Lab Commit

---
+++ 3023-F2021-Labs/Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    float inputX;
+    float inputY;
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(inputX * moveSpeed * Time.deltaTime, inputY * moveSpeed * Time.deltaTime, 0); 
+    }
+}
