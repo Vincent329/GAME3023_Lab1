85fc52c25cc912e7a55e86aca80fc0eeba4949d3
Vincent329 <vincee.ho@gmail.com>
2021-09-20 14:49:22 -0400

    Update PlayerBehaviour.cs

--- 3023-F2021-Labs/Assets/_Scripts/PlayerBehaviour.cs
+++ 3023-F2021-Labs/Assets/_Scripts/PlayerBehaviour.cs
@@ -12,7 +12,7 @@ public class PlayerBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello");
+        
     }
 
     // Update is called once per frame
